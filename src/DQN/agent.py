f10f1e5ae9ed03b35435eaef6c20e649b444bb57
sposusu <b03901079@ntu.edu.tw>
2018-01-20 10:22:35 +0800

    bug fixing

--- src/DQN/agent.py
+++ src/DQN/agent.py
@@ -301,8 +301,8 @@ class NeuralAgent(object):
         holdout_sum = 0
         if self.holdout_data is not None:
             for i in range(holdout_size):
-                holdout_sum += np.max(
-                    self.network.q_vals(self.holdout_data[i, ...]))
+                holdout_sum += 0.001#np.max(
+                    #self.network.q_vals(self.holdout_data[i, ...]))
 
         #self._update_results_file(epoch, self.episode_counter,
         #                          holdout_sum / holdout_size)
